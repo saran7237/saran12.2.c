ec358f4dd30493102b7331cce909ae96d97f6ba1
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-12 15:50:34 +0530

    Create 1pro.12.2.c

---
+++ 1pro.12.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the values");
+scanf("%d",&a);
+for(i=1,i<10,i++)
+{
+printf("enter the values");
+scanf("%d",&b);
+{
+return 0;
+getch();
+}
